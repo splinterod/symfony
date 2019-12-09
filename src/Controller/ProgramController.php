a83dd45718d64628cfc59a032bddf480835d2744
julien <julienl6900669006@gmail.com>
2019-12-09 10:41:53 +0100

    quete17: premiere avancee

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -8,8 +8,11 @@ use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\Slugify;
+
 
 /**
  * @Route("/program")
@@ -29,7 +32,7 @@ class ProgramController extends AbstractController
     /**
      * @Route("/new", name="program_new", methods={"GET","POST"})
      */
-    public function new(Request $request, Slugify $slugify): Response
+    public function new(Request $request, Slugify $slugify, MailerInterface $mailer): Response
     {
         $program = new Program();
         $form = $this->createForm(ProgramType::class, $program);
@@ -40,6 +43,20 @@ class ProgramController extends AbstractController
             $program->setSlug($slugify->generate($program->getTitle()));
             $entityManager->persist($program);
             $entityManager->flush();
+
+
+
+            $email = (new Email())
+                ->from($this->getParameter('mailer_from'))
+                ->to('email@example.com')
+                ->subject('Une nouvelle série vient d\'être publiée !')
+                ->html($this->renderView('program/email/notification.html.twig', [
+                    'program' => $program
+                ]));
+
+            $mailer->send($email);
+
+
 
             return $this->redirectToRoute('program_index');
         }
